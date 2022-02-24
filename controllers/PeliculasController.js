5b4d8516ce776c905db34b57ee47607a1bc08871
jmonloop <monleonlopezjavier@gmail.com>
2022-02-24 22:26:51 +0100

    ordenando funciones

--- controllers/PeliculasController.js
+++ controllers/PeliculasController.js
@@ -11,6 +11,45 @@ const API_KEY = "210d6a5dd3f16419ce349c9f1b200d6d";
 //Declaro el objeto PeliculasController (siempre igual para cada controller)
 const PeliculasController = {};
 
+
+
+//MÉTODO GET PARA MOSTRAR LAS 5 PRIMERAS PÁGINAS DE LAS PELÍCULAS MÁS VALORADAS DE TMDB
+//http://localhost:3000/peliculas/toprated GET
+PeliculasController.traePeliculas = async (req, res) => {
+    const ratedArr = []; //Declaro array vacío que es donde guardaremos el json con el listado de películas
+    try {
+        //Creamos un bucle for e iteramos el valor de page para que nos saque las 5 primeras páginas de rated
+        for(let i=1 ; i<6 ; i++){
+            //llamada por axios al endpoint de TMDB interpolando la API_KEY
+            let results = await axios.get(`https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en-US&page=${i}`);
+            ratedArr.push(results.data)
+        }//Muestro el array de páginas
+        res.send(ratedArr);
+    } catch(error) {
+        res.send(error);
+    };
+};
+
+//MÉTODO GET PARA BUSCAR PELICULA EN TMDB POR TITULO USANDO QUERY
+PeliculasController.peliculasTitulo = async (req, res) => {
+    console.log('entra')
+    //En la variable busqueda guardamos lo que llega por query, es decir:
+    //http://localhost:3000/peliculas/titulo?criterio=TITULO
+    let busqueda = req.query.criterio;
+
+    try {
+        //Hacemos la llamada al endpoint de TMDB interpolando la API_KEY y el valor de búsqueda.
+        let resultados = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${busqueda}&page=1&include_adult=false`);
+
+        res.send(resultados.data);
+        
+
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
 //MÉTODO GET PARA BUSCAR PELICULA EN TMDB POR ID USANDO PARAMS
 PeliculasController.APItraerPeliculaPorId = async (req, res) => {
     //lo que metamos al final del endpoint será la id de la película a buscar en TMDB
@@ -27,59 +66,46 @@ PeliculasController.APItraerPeliculaPorId = async (req, res) => {
 };
 
 
-
-//MÉTODO GET PARA MOSTRAR LAS 5 PRIMERAS PÁGINAS DE LAS PELÍCULAS MÁS VALORADAS DE TMDB
-//http://localhost:3000/peliculas/toprated GET
-PeliculasController.traeTopRatedPeliculas = async (req, res) => {
-    const ratedArr = []; //Declaro array vacío que es donde guardaremos el json con el listado de películas
-    try {
-        //Creamos un bucle for e iteramos el valor de page para que nos saque las 5 primeras páginas de rated
-        for(let i=1 ; i<6 ; i++){
-            //llamada por axios al endpoint de TMDB interpolando la API_KEY
-            let results = await axios.get(`https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en-US&page=${i}`);
-            ratedArr.push(results.data)
-        }//Muestro el array de páginas
-        res.send(ratedArr);
-    } catch(error) {
-        res.send(error);
-    };
-};
-
-
 //MÉTODO POST PARA REGISTRAR PELÍCULA EN BBDD
 PeliculasController.registraPelicula = (req, res) => {
+    //Capturo las variables que llegan por el json de body
     let title = req.body.title;
     let synopsis = req.body.synopsis;
     let adult = req.body.adult;
     let popularity = req.body.popularity;
     let image = req.body.image;
 
-
+    //Hago un findAll de la tabla Pelicula
     Pelicula.findAll({
+        //Donde el atributo title se compara con el title que llega por body
         where : {title : title}
 
     }).then(peliculaRepetida => {
+        //Si no la encuentra (array vacío)...
         if(peliculaRepetida == 0) {
+            //Crea un elemento nuevo en la tabla Pelicula
             Pelicula.create({
+                //guardando en cada atributo los datos que llegan por body
                 title : title,
                 synopsis : synopsis,
                 adult : adult,
                 popularity : popularity,
                 image : image
             }).then(pelicula => {
+                //Y envío un mensaje de confirmación
                 res.send(`La pelicula ${pelicula.dataValues.title}, ha sido registrada correctamente`);
             }).catch((error) => {
                 res.send(error);
             });
+        //Si la encuentra...
         }else {
+            //Envío mensaje de que ya existe en la BBDD
             res.send(`La pelicula ${title}, ya está registrada en la base de datos`);
         }
         
     }).catch(error => {
         res.send(error)
     });
-};
-
 // {
 //     "title": "El silencio de los corderos",
 //     "synopsis": "Un tío muy carismático que come personas",
@@ -87,6 +113,7 @@ PeliculasController.registraPelicula = (req, res) => {
 //     "popularity": 7.3,
 //     "image": "stringIMAGE"
 // }
+};
 
 
 
@@ -95,22 +122,9 @@ PeliculasController.registraPelicula = (req, res) => {
 
 
 
-PeliculasController.peliculasTitulo = async (req, res) => {
 
-    let busqueda = req.query.criterio;
 
-    try {
 
-        let resultados = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=210d6a5dd3f16419ce349c9f1b200d6d&language=en-US&query=${busqueda}&page=1&include_adult=false`);
-
-        res.send(resultados.data);
-        
-
-    } catch (error) {
-        console.log(error);
-    }
-
-}
 
 PeliculasController.traeNovedades = async (req, res) => {
 
--- views/PeliculasRouter.js
+++ views/PeliculasRouter.js
@@ -6,29 +6,24 @@ const router = express.Router();
 const PeliculasController = require('../controllers/PeliculasController');
 
 //Enlazo método(CRUD), endpoint y función. (Explicación en UsuariosRouter)
-//http://localhost:3000/peliculas/toprated
+//http://localhost:3000/peliculas
 //Coge las películas más votadas de TMDB (5 primeras páginas)
-router.get('/toprated', PeliculasController.traeTopRatedPeliculas);
+router.get('/', PeliculasController.traePeliculas);
+
+//http://localhost:3000/peliculas/titulo?criterio=tituloPelicula
+//Traer peliculas de TMDB por titulo usando query
+router.get('/titulo', PeliculasController.peliculasTitulo);
 
 //http://localhost:3000/peliculas/idPeliculaEnTMDB GET
-//Traer datos de una pelicula buscándola por su id de TMDB
+//Traer datos de una pelicula buscándola por su id de TMDB usando params
+//OJO! Si este endpoint lo pusiésemos arriba de el de buscar por titulo, JS al leer la URL cree que 'titulo' es un id y da fallo.
+//Es por ello que al usar params hay que tener cuidado de no ponerlos en endpoints comunes o ponerlos al final del todo del xxxRouter
 router.get('/:id', PeliculasController.APItraerPeliculaPorId);
 
 //http://localhost:3000/peliculas
 //Registrar una peli nueva
 router.post('/', PeliculasController.registraPelicula);
 
-//http://localhost:3000/peliculas/favoritas
-//Búsqueda de películas favoritas
-router.get('/favoritas', PeliculasController.favouriteFilms);
-
-//http://localhost:3000/peliculas/adultos
-//Búsqueda de películas si son de adultos
-router.get('/adultos', PeliculasController.peliculasAdultas);
-
-//http://localhost:3000/peliculas/titulo
-//Búsqueda de películas por título
-router.get('/titulo', PeliculasController.peliculasTitulo);
 
 //http://localhost:3000/peliculas/novedades
 //Búsqueda de novedades
