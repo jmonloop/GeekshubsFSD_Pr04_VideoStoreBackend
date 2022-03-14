8bb0de4f93be9107760d4e24dd6743e6df1a0890
jmonloop <monleonlopezjavier@gmail.com>
2022-03-14 13:50:41 +0100

    create order isAdmin removed

--- views/OrdersRouter.js
+++ views/OrdersRouter.js
@@ -5,7 +5,7 @@ const OrdersController = require('../controllers/OrdersController');
 const isAdmin = require('../middlewares/isAdmin');
 
 ////Enpoint-function links
-router.post('/', isAdmin, OrdersController.createByBody);
+router.post('/', OrdersController.createByBody);
 router.post('/query', OrdersController.createByQuery);
 router.get('/', OrdersController.fullReport);
 router.get('/:id', OrdersController.reportByUserId);
