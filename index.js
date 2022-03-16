f6f86be17eca2f846d789774780b6ad404552c94
jmonloop <monleonlopezjavier@gmail.com>
2022-03-16 12:03:17 +0100

    all cors reverted to default

--- index.js
+++ index.js
@@ -12,9 +12,10 @@ const PORT = process.env.PORT || 5000;
 //Options for CORS configuration
 let corsOptions = {
     origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    // methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
     preflightContinue: false,
-    allowedHeaders: "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+    // allowedHeaders: "Origin,X-Requested-With,Content-Type,Accept,Authorization",
     optionsSuccessStatus: 204
 };
 
